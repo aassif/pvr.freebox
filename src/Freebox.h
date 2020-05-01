d2a27b4900955b42981a945feeeb27fceba9331c
Aa! <aa@bns.sr>
2020-05-01 11:15:39 +0200

    2.0.3

--- src/Freebox.h
+++ src/Freebox.h
@@ -30,7 +30,7 @@
 #include "p8-platform/threads/threads.h"
 #include "rapidjson/document.h"
 
-#define PVR_FREEBOX_VERSION "2.0.2"
+#define PVR_FREEBOX_VERSION "2.0.3"
 
 #define PVR_FREEBOX_BACKEND_NAME "Freebox TV"
 #define PVR_FREEBOX_BACKEND_VERSION PVR_FREEBOX_VERSION
